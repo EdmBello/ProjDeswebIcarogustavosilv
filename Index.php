30ca05462e119654c2f58f4fcac5a9a17f7fb6fa
Cheat.co <146290154+EdmBello@users.noreply.github.com>
2023-11-04 20:23:19 -0300

    footer

--- Index.php
+++ Index.php
@@ -75,4 +75,13 @@ if(isset($_POST['User']) || isset($_POST['senha'])) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </main>
 </body>
+<footer>
+    <footer id="myFooter">
+        <div class="container #0cccf4">
+            <div class="row">
+                <div class="row-cols-sm-9">
+    <div class="footer-copyright">
+            <h5>© 2023 Copyright - Icaro Belo & Gustavo silva.</h5>
+        </div>
+</footer>
 </html>
